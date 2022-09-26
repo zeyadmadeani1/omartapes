fa3ae315d165b5a91630544f9703b923b5046bdb
unknown <omar_kanan@outlook.com>
2022-09-26 03:38:20 -0600

    my first commit

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 export const axiosInstance=axios.create(
     {
-        baseURL:"http://omartube.herokuapp.com/api/"
+        baseURL:"https://omartube.herokuapp.com/api/"
     })
